e2ffefef23667649d01722fa8e1c397afbad9896
Travis. <youngtravislicious@gmail.com>
2024-08-31 00:06:53 +0000

    "Updated styling and layout for Download and LoadingDownload pages, adding md breakpoints and adjusting font sizes, widths, and gaps."

--- client/src/pages/Download.tsx
+++ client/src/pages/Download.tsx
@@ -69,36 +69,36 @@ export default function DownloadPage() {
             <main className="w-screen h-screen flex flex-col p-4 items-center justify-center">
                 <header className="w-full flex items-center justify-between">
                     <a href="/" role="button">                  
-                        <ArrowLeft/>
+                        <ArrowLeft className="md:size-8"/>
                     </a>
                     <Share2 onClick={() => {navigator.clipboard.writeText("http://localhost:5173")
                         alert("Link Copied.")
                     }}/>
                 </header>
                 <section className="w-full h-full flex flex-col items-center justify-center gap-5">
-                    <section className="w-full flex items-center gap-4">
+                    <section className="w-full flex items-center gap-4 md:w-96">
                         <div className="flex flex-col items-center justify-between gap-2">
-                            <div className="w-16 h-16 rounded bg-primary items-center justify-center flex">
+                            <div className="w-16 h-16 rounded bg-primary items-center justify-center flex md:w-18 md:h-18">
                                 <Music/>
                             </div>
-                            <h2 className="font-semibold">00:{duration ? duration : "00"}</h2>
+                            <h2 className="font-semibold md:text-lg">00:{duration ? duration : "00"}</h2>
                         </div>
-                        <div className="w-full flex flex-col justify-center gap-1">
-                            <h1 className="text-2xl font-bold truncate w-64">{data?.song_name}</h1>
-                            <h1 className="text-xl">{data?.music_author}</h1>
-                            <h2 className="text-sm truncate w-70">Extracted from <span className="text-accent font-bold">{data?.video_author}'s</span> Video.</h2>
+                        <div className="w-full flex flex-col justify-center gap-1 md:gap-2">
+                            <h1 className="text-2xl font-bold truncate w-64 md:text-4xl">{data?.song_name}</h1>
+                            <h1 className="text-xl md:text-2xl">{data?.music_author}</h1>
+                            <h2 className="text-sm truncate w-70 md:text-base">Extracted from <span className="text-accent font-bold">{data?.video_author}'s</span> Video.</h2>
                         </div>
                     </section>
-                    <section className="flex w-full items-center justify-center flex-col gap-3">
-                        { !videoPath ? (<button className="btn btn-primary w-full font-bold text-lg" disabled={isProcessing} onClick={processVideo}>{ isProcessing ? "Processing Video" : "Process Video"}</button>) : (
-                            <a href={`http://127.0.0.1:5000/download-video?path=${videoPath}&name=${data?.video_filename}`} target="_blank" role="button" className="btn w-full font-bold text-lg btn-primary">Download Video</a>
+                    <section className="flex w-full items-center justify-center flex-col gap-3 md:w-96">
+                        { !videoPath ? (<button className="btn btn-primary w-full font-bold text-lg md:text-xl" disabled={isProcessing} onClick={processVideo}>{ isProcessing ? "Processing Video" : "Process Video"}</button>) : (
+                            <a href={`http://127.0.0.1:5000/download-video?path=${videoPath}&name=${data?.video_filename}`} target="_blank" role="button" className="btn w-full font-bold text-lg btn-primary md:text-xl">Download Video</a>
                         )}
-                        { !isProcessing && (<a role="button" href={`http://127.0.0.1:5000/download-song?url=${data?.music_url}&name=${data?.audio_filename}`} target="_blank" className="btn w-full font-bold text-lg">Download Audio.</a>)}
-                        { videoPath && <h6 className="text-sm text-white/30 w-full text-center">Processed Videos are deleted after 2 minutes.</h6>}
+                        { !isProcessing && (<a role="button" href={`http://127.0.0.1:5000/download-song?url=${data?.music_url}&name=${data?.audio_filename}`} target="_blank" className="btn w-full font-bold text-lg md:text-xl">Download Audio.</a>)}
+                        { videoPath && <h6 className="text-sm text-white/30 w-full text-center md:text-base">Processed Videos are deleted after 2 minutes.</h6>}
                     </section>
                 </section>
                 <footer className="w-full">
-                    <h1 className="text-sm italic text-center w-full text-white/30">Extractions are based on the TikTok Song Extraction System and may be imprecise.</h1>
+                    <h1 className="text-sm italic text-center w-full text-white/30 md:text-base">Extractions are based on the TikTok Song Extraction System and may be imprecise.</h1>
                 </footer>
             </main>
         )
--- client/src/pages/LoadingDownload.tsx
+++ client/src/pages/LoadingDownload.tsx
@@ -6,7 +6,7 @@ export default function Loading() {
                     <div className="w-10 h-10 skeleton"></div>
                     <div className="w-10 h-10 skeleton"></div>
                 </header>
-                <section className="w-full h-full flex flex-col items-center justify-center gap-5">
+                <section className="w-full h-full flex flex-col items-center justify-center gap-5 md:w-96">
                     <section className="w-full flex items-center gap-4">
                         <div className="flex flex-col items-center justify-between gap-2">
                             <div className="w-16 h-16 skeleton">
@@ -19,13 +19,13 @@ export default function Loading() {
                             <div className="w-full h-6 skeleton"></div>
                         </div>
                     </section>
-                    <section className="flex w-full items-center justify-center flex-col gap-3">
+                    <section className="flex w-full items-center justify-center flex-col gap-3 md:w-96">
                         <div className="w-full h-14 skeleton"></div>
                         <div className="w-full h-14 skeleton"></div>
                     </section>
                 </section>
-                <footer className="w-full">
-                <div className="w-full h-10 skeleton"></div>
+                <footer className="w-full flex items-center justify-center">
+                <div className="w-full h-10 skeleton md:w-[40rem]"></div>
 
                 </footer>
             </main>
